642f65006edbb024c2df54d0940c32ac60a87da9
jeffslutzky <jeffslutzky@gmail.com>
2015-08-19 11:29:25 -0400

    done

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  n = 0
+  loop do
+    n += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    break if n == number_of_times
+  end
+
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -13,12 +20,20 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+  number_of_times.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
 def while_iterator(number_of_times)
+  n = 0
+  while n < number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+    n += 1
+  end
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -26,6 +41,11 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
+  n = 0
+  until n == number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+    n += 1
+  end
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -33,6 +53,9 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
+  for n in (1..number_of_times)
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
