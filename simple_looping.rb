fccafaf59f6c95fb16d9c2751ac5db0407260440
simque <simon.duncanson@gmail.com>
2015-10-23 17:00:25 -0400

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,14 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop_iterator = 0
+  loop do
+    puts "#{phrase}"
+    loop_iterator += 1
+    break if loop_iterator == 7
+  end
+end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -8,35 +16,38 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-end
+
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times.times do
+    puts "#{phrase}"
+  end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
+  counter = 1
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  while counter <= 7
+    puts "#{phrase}"
+    counter += 1
+  end
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
+  counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  until counter == 7
+    puts "#{phrase}"
+    counter += 1
+  end
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+  number_of_times = 1..7
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for say_spell in number_of_times
+    puts "#{phrase}"
+  end
 end
 
