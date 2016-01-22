9b51f29a3dee1c737eef43dab962e05c9fdc7097
Goran Karic <g.karic88@gmail.com>
2016-01-22 10:32:14 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,7 +7,7 @@ def loop_iterator(number_of_times)
   puts "Welcome to Flatiron School's Web Development Course!"# Code your solution here using the "loop" keyword to puts out the below phrase
     # This and all the other methods take an argument of an integer
     # The integer is the number of times the loops should puts out the phrase
-    if number_of_times >= 7# How can we make sure the loop breaks once it has puts out the phrase the
+    if number_of_times == 7# How can we make sure the loop breaks once it has puts out the phrase the
       break#   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the
   #  phrase and break when the counter hits the appropriate number...
@@ -35,7 +35,7 @@ end
 
 def until_iterator(number_of_times)
   number_of_times = 0
-until number_of_times == 7 
+until number_of_times == 7
   puts "Welcome to Flatiron School's Web Development Course!"# code your solution here using the "until" keyword
   number_of_times += 1# hint: use a counter to tell the until loop when to stop!
   end
