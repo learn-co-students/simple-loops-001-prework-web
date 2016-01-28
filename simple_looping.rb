c525cbe584eb18e3c88f31f2aae40583b03bc1ca
mlobasso <mike.lobasso@gmail.com>
2016-01-28 23:09:48 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,7 +16,7 @@ def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
   number_of_times.times do
-    puts phrase
+    puts phrase 
   end
 end
 
