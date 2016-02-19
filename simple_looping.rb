0b25878723ee548e570fe13e532b690445d87524
LibraHampton <LibraHampton@live.com>
2016-02-19 11:28:40 +0000

    The tests passed but I feel the code was not correct.  I am submitting some changes.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,13 +1,13 @@
-def loop_iterator(number_of_times = 7)
+def loop_iterator(number_of_times = 0)
 
 loop do
   number_of_times = number_of_times + 1
   puts "Welcome to Flatiron School's Web Development Course!"
-  break if number_of_times <= 7
+  if number_of_times == 7
+    break
 end
 end
-
-
+end
 
 
 def times_iterator(number_of_times = 7)
@@ -15,10 +15,6 @@ def times_iterator(number_of_times = 7)
   puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
-
-
-
-
 
 
 def while_iterator(number_of_times = 0)
@@ -33,9 +29,7 @@ end
 while_iterator
 
 
-
-
-def until_iterator(number_of_times = 7)
+def until_iterator(number_of_times = 0)
 
     until number_of_times >= 7
     puts "Welcome to Flatiron School's Web Development Course!"
@@ -45,15 +39,7 @@ def until_iterator(number_of_times = 7)
 end
 
 
-
-
-
-
-
-
-
-
-def for_iterator(number_of_times = 7)
+def for_iterator(number_of_times = 1..7)
 
   for number in number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
