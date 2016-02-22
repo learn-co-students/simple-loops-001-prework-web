5d60084bcd714e7564a908e393d696845568b9a7
motti770 <mottigottlieb@gmail.com>
2016-02-22 23:02:32 +0000

    nitrous-backup at Mon Feb 22 23:02:32 UTC 2016

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,6 +23,9 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.do 
+  puts phrase
+end
 
 end
 
@@ -30,6 +33,7 @@ def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  
 
 end
 
