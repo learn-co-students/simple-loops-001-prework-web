db101e1ec63faea95a9ca2a7b5c00d0aa01e050d
samanthabertous <sam@samanthabertous.com>
2016-04-20 07:28:47 -0400

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,13 +8,25 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  looping_phrase = 0 
+  loop do 
+    looping_phrase += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+      if looping_phrase >= number_of_times
+        break
+      end
+  end
+   
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   
 end
 
@@ -22,6 +34,11 @@ def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0;
+  while counter < number_of_times do
+    puts phrase
+    counter +=1
+  end
   
 end
 
@@ -29,6 +46,12 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+   until counter == number_of_times do
+      puts phrase
+      counter +=1
+    end
+
 
 end
 
@@ -37,6 +60,13 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 1..number_of_times
+  for count in counter
+    puts phrase
+  end
   
 end
+
+#loop_iterator(10)
+for_iterator(10)
 
