d0e1751e90b18cdb1a3be6cdfef959adea0ac687
tiantianshi <tiantianshi08@gmail.com>
2016-05-28 18:55:58 -0400

    Done.

--- untitled.rb
+++ untitled.rb
@@ -5,7 +5,6 @@ def loop_iterator(number_of_times)
  loop do
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
-  break if counter == number_of_times
   end
 end
 
@@ -16,18 +15,16 @@ end
 end
 
 def while_iterator(number_of_times)
- counter = 0
- while counter < number_of_times
-  puts "Welcome to Flatiron School's Web Development Course!"
-  counter +=1
-end
+  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  
 end
 
 def until_iterator(number_of_times)
- counter = 0
  until counter == number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
-  counter += 1
+  number_of_times += 1
 end
 end
 
@@ -36,3 +33,4 @@ def for_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
+
