2e1446b73b651bee7dd05dc55745aa5a250ebc06
craigschiff <craig.schiff@gmail.com>
2016-07-03 17:18:29 -0400

    done

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,17 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
+
 def loop_iterator(number_of_times)
+  number_of_times = 1
+  loop do
+    number_of_times += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times > 7
+          break
+      end
+  end
+  
+
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -8,13 +19,16 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    7.times do
+      puts phrase
+  end
   
 end
 
@@ -22,6 +36,11 @@ def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  num = 0
+  while num < 7
+      num += 1
+      puts phrase
+      end
   
 end
 
@@ -29,6 +48,11 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  num = 0
+  until num > 6
+      num += 1
+      puts phrase
+      end
 
 end
 
@@ -37,6 +61,9 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  for num in 1..7 do
+      puts phrase
+      end
   
 end
 
