2ad43b6c3ad3c2d50acef1e10accfd4882833ca9
elimiller783 <emiller783@gmail.com>
2016-07-13 16:02:36 -0400

    simple loops are done

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,35 +8,48 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+    loop do
+     counter += 1
+     puts "#{phrase}"
+     break if counter == number_of_times
+    end
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+   (number_of_times).times do
+     puts "#{phrase}"
+  end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
+  counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+   while counter < number_of_times
+    counter += 1
+      puts "#{phrase}"
+  end
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
+  counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+    until counter == number_of_times
+      counter += 1
+      puts "#{phrase}"
+    end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+    number = (0...number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+    for num in number
+      puts "#{phrase}"
+    end
 end
 
