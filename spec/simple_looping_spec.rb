5af534ec79673ce47c6e8f434ea863d84f561df6
Michael Wiggins <michael.wiggins@me.com>
2016-07-27 14:48:03 +0000

    Done.

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -2,8 +2,13 @@ require_relative './spec_helper'
 
 describe 'Loops' do
   before(:each) do
-    @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
+    @looping_string = "Welcome to Flatiron School's Web Development Course!"
   end
+
+  it "prints out Welcome to Flatiron School's Web Development Course! 7 times" do
+    expect(@looping_string).to eq("Welcome to Flatiron School's Web Development Course!")
+  end
+end
 
   describe 'loop' do
     it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
@@ -34,4 +39,3 @@ describe 'Loops' do
       expect{ for_iterator(7) }.to output(@looping_string).to_stdout
     end
   end
-end
